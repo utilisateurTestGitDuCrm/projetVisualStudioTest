64fa8974dfa135d1ad5341c4e4a43fcc5b30ad5f
utilisateurTestGitDuCrm <sophie.thiry.git@gmail.com>
2017-10-02 16:17:22 +0200

    ajout du bouton

--- WindowsFormsApplicationTest1/WindowsFormsApplicationTest1/Form1.Designer.cs
+++ WindowsFormsApplicationTest1/WindowsFormsApplicationTest1/Form1.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(280, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(652, 429);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +53,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
