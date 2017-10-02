0770f0aa65a5bbd2dc9e7e3446d6bf66915c1fed
utilisateurTestGitDuCrm <32102682+utilisateurTestGitDuCrm@users.noreply.github.com>
2017-10-02 16:22:22 +0200

    changement sur github

--- WindowsFormsApplicationTest1/WindowsFormsApplicationTest1/Form1.Designer.cs
+++ WindowsFormsApplicationTest1/WindowsFormsApplicationTest1/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(107, 61);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "lebouton1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
